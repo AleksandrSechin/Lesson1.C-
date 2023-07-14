fe5f12e5e21e21cbea4036cb37696e2824b889dc
Alex <ansechin2012@ya.ru>
2023-07-14 11:31:04 +0300

    Задача 11_2

---
+++ Задача 11_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98
+
+
+Console.WriteLine("Генерируем трехзначное число");
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+int number1 = number % 10;
+int number2 = number / 100;
+
+Console.WriteLine($"Вывести первую и последнюю цифру числа {number2}{number1}");
