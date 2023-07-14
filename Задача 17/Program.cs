eacb2b091e6e5d6f9ab99f778d55ecaf39ae6174
Alex <ansechin2012@ya.ru>
2023-07-14 19:07:06 +0300

    Задача18

---
+++ Задача 17/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход координаты
+// точки (X и Y), где Х и Y не равны 0 и выдает номер слоскости
+// в которой находится эта точка
+
+
+Console.Write("Введите Х: ");
+int numberX = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int numberY = Convert.ToInt32(Console.ReadLine());
+
+if (numberX == 0) Console.Write("X = 0 ");
+if (numberY == 0) Console.Write("Y = 0 ");
+
+if (numberX > 0 && numberY > 0)
+{
+    Console.Write("I четверть ");
+}
+else if (numberX < 0 && numberY > 0)
+{
+    Console.Write("II четверть ");
+}
+else if (numberX < 0 && numberY < 0)
+{
+    Console.Write("III четверть ");
+}
+else if (numberX > 0 && numberY < 0)
+{
+    Console.Write("IV четверть ");
+}
