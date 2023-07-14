8751c84ab03ad732c57a196a48772cf031dc70b2
Alex <ansechin2012@ya.ru>
2023-07-14 20:29:37 +0300

    Задача 21

--- Задача 21/Program.cs
+++ Задача 21/Program.cs
@@ -2,7 +2,7 @@
 //  двух точек и находит расстояние между ними в 2D пространстве.
 //  A (3,6); B (2,1) -> 5,09    A (7,-5); B (1,-1) -> 7,21
 
-Console.Clear();
+Console.Clear();  // очистка консоли
 
 Console.Write("Введите x1: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
@@ -13,7 +13,7 @@ int x2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите y2: ");
 int y2 = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)); // складываем возведенные в степень значения
 
 Console.WriteLine(result); 
 // или Console.WriteLine($"{Math.Round(result, 1)}");
