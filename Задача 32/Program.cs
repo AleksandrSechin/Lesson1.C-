fe7a5d879a869670090a5541ad7e9a76c2326339
Alex <ansechin2012@ya.ru>
2023-07-18 15:34:24 +0300

    Задача 32 замена знака в элементах

---
+++ Задача 32/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные,
+//  и наоборот.[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] numbers = new int[12];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(-9, 10);
+    Console.Write(numbers[i] + " ");
+}
+Console.WriteLine();
+
+for (int j = 0; j < numbers.Length; j++)
+{
+    numbers[j] = numbers[j] * -1;
+    Console.Write(numbers[j] + " ");
+}
