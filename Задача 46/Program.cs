248c411bab26cf0fc593b1ab8e0dd65c1fe8176f
Alex <ansechin2012@ya.ru>
2023-07-25 22:18:30 +0300

    Задача 46 создать двумерный массив

--- Задача 46/Program.cs
+++ Задача 46/Program.cs
@@ -24,3 +24,28 @@ for (int i = 0; i < numbers.GetLength(0); i++)
     }
     Console.WriteLine();
 }
+
+// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] nums = new int[3, 4];
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        nums[i, j] = i + j;
+    }
+}
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
