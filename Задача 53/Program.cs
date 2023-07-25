1e8ad564a4e7984e3d6d162a00404c640853b690
Alex <ansechin2012@ya.ru>
2023-07-25 23:17:28 +0300

    Задача 53 заменить местами строки двумерного массива

---
+++ Задача 53/Program.cs
@@ -0,0 +1,49 @@
+﻿//Задача 53:** Задайте двумерный массив. Напишите программу, 
+// которая поменяет местами первую и последнюю строку массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 8 4 2 4
+// 5 9 2 3
+// 1 4 7 2
+
+
+int[,] nums = new int[3, 4];
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        nums[i, j] = new Random().Next(0, 10);
+    }
+}
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < nums.GetLength(1); i++)
+{
+    {
+        int temp = nums[0, i];
+        nums[0, i] = nums[nums.GetLength(0) - 1, i];
+        nums[nums.GetLength(0) - 1, i] = temp;
+    }
+}
+Console.WriteLine();
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
