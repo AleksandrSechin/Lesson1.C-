8cb5c31cc74808bb1608533066e19ef46b1e869a
Alex <ansechin2012@ya.ru>
2023-07-25 15:51:57 +0300

    Задача 42 десятичное в двоичное

---
+++ Задача 42/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.45 -> 1011013 -> 112 -> 10
+
+Console.WriteLine("Введите десятичное число: ");
+int number = Int32.Parse(Console.ReadLine());
+int numberBefore = number;
+string result = String.Empty;
+
+while (number > 0)
+{
+    result = Convert.ToString(number % 2) + result;
+    number /= 2;
+}
+Console.WriteLine($"Число {numberBefore} в двоичной системе {result}");
