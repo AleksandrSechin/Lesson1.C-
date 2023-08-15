660da751a137a19da78c7f881c3e94bfe3e81359
Alex <ansechin2012@ya.ru>
2023-08-15 19:51:27 +0300

    Наибольшая цифра числа из отрезка

---
+++ Задача 11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит случайное число 
+// из отрезка [10, 99] и показывает наибольшую цифру числа. 
+// Например:78 -> 8  12-> 2  85 -> 8 
+
+Console.WriteLine("Генерируем двухзначное число");
+
+int number = new Random().Next(10, 99);
+
+Console.WriteLine(number);
+
+int number1 = number % 10;
+int number2 = number /= 10;
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Наибольшая цифра {number1}");
+}
+else Console.WriteLine($"Наибольшая цифра {number2}");
