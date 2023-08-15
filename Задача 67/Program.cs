92822783964a07b4d066dfb5e35e061a6bdb16a4
Alex <ansechin2012@ya.ru>
2023-08-15 21:06:02 +0300

    Задача 67 сумма цифр числа через рекурсию

---
+++ Задача 67/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать
+//  сумму его цифр.453 -> 1245 -> 9
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int Sum(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + Sum(num / 10);
+}
+int number = Sum(num);
+Console.WriteLine(number);
